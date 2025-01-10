de07f15006b5de1c1f17d54b91868b6d49bab2de
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-01-10 22:10:28 +0530

    Add files via upload

---
+++ ascinarr.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class ascinarr {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the array:");
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Original Array is:");
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(+a[i]);
+        }
+        System.out.println("Convert into string:");
+      System.out.println(Arrays.toString(a));
+      Arrays.sort(a);
+      
+      System.out.println("Sorted array in ascending order"+Arrays.toString(a));
+    }
+    
+}
