ee1d00e3f546cbdc6e93d529e6b6eea30bdd37f7
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-01-27 19:01:15 +0530

    Add files via upload

---
+++ final_opr.java
@@ -0,0 +1,18 @@
+public class final_opr {
+    final int a=10;
+    void get()
+    {
+        System.out.println(a);
+    }
+    void dis()
+    {
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        final_opr obj1 = new final_opr();
+        obj1.get();
+        obj1.dis();
+    }
+
+    
+}
