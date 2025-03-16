a9879a461bb2798ae3cd097f81b020b0fc771edc
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-03-16 20:15:04 +0530

    Add files via upload

---
+++ primitivechall.java
@@ -0,0 +1,11 @@
+public class primitivechall {
+    public static void main(String args[])
+    {
+        byte r=10;
+        short r1=20;
+        int r2=30;
+        long a=50000l;
+        System.out.println( a=a+10*(r+r1+r2));
+    }
+    
+}
