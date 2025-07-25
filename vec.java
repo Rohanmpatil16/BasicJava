100b2e8b0fc99454efb13a9c179458693cb75fe9
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-07-25 23:46:36 +0530

    Add files via upload

---
+++ vec.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class vec
+{
+public static void main(String args[])
+{
+Vector v1=new Vector();
+v1.add(10);
+v1.addElement("RP");
+v1.insertElementAt("RR",2);
+System.out.println(v1);
+}
+}
