624160e8d120bb40f9a95f28ced8f8fcb9aaaf5e
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-08-15 21:40:27 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,11 @@
+class str
+{
+	public static void main(String args[])
+	{
+	String r="rohannnnnnn patilllllllll";
+	String rp=new String("welcome to the jungle....");
+	System.out.println(r);
+    System.out.println(rp);
+
+	}
+}
