b775b01f7f84758bc17cc9395c45e2e0065b8fad
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2026-05-06 22:31:22 +0530

    Add files via upload

---
+++ base1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class base1
+{
+	public static void main(String args[])
+	{
+		int a,sq;
+		Scanner r=new Scanner(System.in);
+		a=r.nextInt();
+		sq=a*a;
+		System.out.println(sq);
+		
+	}
+}
