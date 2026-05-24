cd1814ab09cf25e0f15f4aaf2e17427444132c62
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-05-24 22:26:57 +0530

    code of simple stars and variable programs

---
+++ Java2026/vari.java
@@ -0,0 +1,14 @@
+class vari{
+    public static void main(String args[])
+    {
+        int a=10;
+        String name="john";
+        char ch='a';
+        float f=3.14f;
+        double d=0.00001;
+        boolean b=true;
+
+        System.out.println(a+"\n"+name+"\n"+ch+"\n"+f+"\n"+d+"\n"+b);
+
+    }
+}
