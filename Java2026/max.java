f3f3b0e21d2fcffe2aa897a95a0aee7f8c38d5b9
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-05-31 19:18:04 +0530

    Added a code of array finding biggest number

---
+++ Java2026/max.java
@@ -0,0 +1,21 @@
+public class max {
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 8, 45, 32};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest element: " + max);
+    }
+}
+    
+
